27d9afddb489772495af6a92162ba2412e6a7190
Doug Diego <dougdiego@gmail.com>
2024-12-19 10:59:42 -0800

    Enhance rating processing by cleaning review counts and improving output formatting

--- app-store-ratings/ratings.py
+++ app-store-ratings/ratings.py
@@ -14,7 +14,12 @@ def get_google_play_rating(app_id):
     rating = soup.find('div', class_='TT9eCd').text
     clean_rating = rating.replace("star", "")
     count = soup.find('div', class_='g1rdde').text
-    return clean_rating, count
+    # Clean up the count string
+    clean_count = count.replace(" reviews", "").replace("reviews", "").replace(",", "").strip()
+    if 'K' in clean_count:
+        clean_count = float(clean_count.replace('K', '')) * 1000
+        clean_count = int(clean_count)
+    return clean_rating, clean_count
     
 def get_apple_store_rating(bundle_id):
 	# http://itunes.apple.com/lookup?bundleId=com.yelp.yelpiphone
@@ -31,13 +36,15 @@ def process_apps():
         data = json.load(file)
         
     for app in data['apps']:
-        print(f"\nProcessing ratings for {app['name']}:")
+        print(f"\n{app['name']}")
+        print("| App    | Ratings | Reviews |")
+        print("| ------ | ------- | ------- |")
         
         google_rating, google_count = get_google_play_rating(app['google_play_id'])
         apple_rating, apple_count = get_apple_store_rating(app['apple_store_id'])
         
-        print(f"Google Play Rating: {google_rating} Count: {google_count}")
-        print(f"Apple App Store Rating: {apple_rating} Count: {apple_count}")
+        print(f"| Google | {google_rating:<7} | {google_count:<7} |")
+        print(f"| Apple  | {apple_rating:<7.2f} | {apple_count:<7} |")
 
 if __name__ == "__main__":
     process_apps()
